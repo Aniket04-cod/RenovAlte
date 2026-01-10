82d7162e0f7b7a4d30c59b6e4da54f69a75be5db
Abdul Rehman <smabdulrt@gmail.com>
2026-01-10 16:53:45 +0100

    Make the new migrations

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	path("", include("core.api.contractors.urls")),
 	path("", include("core.api.auth.urls")),
 	path("", include("core.api.financing.urls")),
-	path("renovation/", include("core.api.planning_work.urls")),
+#	path('renovation/', include('core.api.planning_work.urls')),
 	path("chatbot/", include("core.api.chatbot.url")),
 	path("", include("core.api.auth.urls")),
 	path("contracting/", include("core.api.contracting.urls")),
